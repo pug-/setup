8dea53e76fb6cd77ff6e980de5c7f31d134061d5
Cameo Admin <itc.admin@cameoml.com>
2013-07-31 01:03:16 +0530

    changed the dotfiles repo source to a pug- repo. Couple of other changes

--- setup.sh
+++ setup.sh
@@ -39,10 +39,15 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+
+# Clone dotfiles from forked repo at pug-
+git clone https://github.com/pug-/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
+
+# Additional dev software 
+sudo apt-get install -y vim
 
